36b6a31a6505759379c91c3e3dafad31da5ebc13
Alec Miller <alecmichaelmiller@gmail.com>
2021-12-04 19:04:10 -0700

    check for score updates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import {getTeams, getTeamIdFromNumber} from "./teams"
+import { getTeams } from "./teams"
+import { getMatches as checkForScores } from "./matches";
 
 const hostname = "localhost"
 const division = "division1"
 
+async function scoreUpdater() {
+    const newScore = await checkForScores(hostname, division);
+    if(newScore){
+        console.log(newScore);
+    }
+
+    return
+}
+
 async function doTheThing() {
     const teams = await getTeams(hostname, division);
 
-    console.log(getTeamIdFromNumber(teams['4'].number));
+    setInterval(scoreUpdater, 500);
 }
 
 doTheThing();
