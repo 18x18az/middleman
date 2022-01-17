48f869fc4c5786bf840b71c2ffbcd7400785a147
Josh T <joshliam09@gmail.com>
2022-01-17 15:48:01 -0700

    auth is pain

--- src/awards.ts
+++ src/awards.ts
@@ -1,9 +1,10 @@
-import { getTable } from "./request";
+import { getAuthTable, getTable } from "./request";
 import { getTeamIdFromNumber } from "./teams";
 
 export async function getAwards(hostname: string, division: string): Promise<Array<string>> {
-    const raw = await getTable(`http:${hostname}/${division}/awards`);
-    
+    //const raw = await getTable(`http:${hostname}/${division}/awards`);
+    const raw = await getAuthTable('/awards', division, `http:${hostname}/${division}/awards`);
+    console.log("boop")
     const winningTeams = raw.map((row: any) => {
         const columns = Array.from(row.cells).map((cell: any) => (cell.textContent));
         const teamId = getTeamIdFromNumber(columns[1]);
--- src/request.ts
+++ src/request.ts
@@ -21,3 +21,34 @@ export function getTable(url: string): Promise<Array<any>> {
         });
     });
 }
+
+const hostname = process.env.TM_HOSTNAME as string;
+const password = process.env.TM_PASSWORD as string;
+
+export function getAuthTable(path: string, division: string, url: string): Promise <Array<any>> {
+    const options = {
+       headers: {
+        auth: `admin:${password}`,
+
+       }
+
+    };
+    return new Promise((resolve, reject) => {
+        http.get(url, options, (resp) => {
+            let data = '';
+            
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            resp.on('end', () => {
+                const dom = new jsdom.JSDOM(data);
+                const contents = Array.from(dom.window.document.querySelector('tbody').rows);
+                resolve(contents)
+            });
+
+        }).on("error", (err) => {
+            reject(err.message);
+        });
+    });
+}
