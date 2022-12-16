8034322ab52a287406450ad59d211a247d43a2f7
Joshua Tenorio <joshliam09@gmail.com>
2022-12-15 21:26:41 -0700

    assign matches to a schedule block

--- src/schedule.ts
+++ src/schedule.ts
@@ -5,6 +5,7 @@ config();
 interface ScheduleBlock {
     start: Date,
     stop: Date,
+    matches: number[]
 }
 
 const db = new sqlite3.Database(process.env.DB_PATH as string, sqlite3.OPEN_READWRITE);
@@ -12,21 +13,34 @@ const db = new sqlite3.Database(process.env.DB_PATH as string, sqlite3.OPEN_READ
 export async function parseScheduleBlocks(): Promise<ScheduleBlock[]> {
     return new Promise<ScheduleBlock[]>(resolve => {
         let blocks: ScheduleBlock[] = [];
-        db.all('SELECT * FROM schedule_blocks', (err: any, rows: any) => {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            console.log(rows);
-            rows.forEach((element: any) => {
-                let start: Date = new Date(element.start * 1000);
-                let stop: Date = new Date(element.stop * 1000);
-                let block: ScheduleBlock = {
-                    start, stop
+        db.serialize(() => {
+            db.all('SELECT * FROM schedule_blocks', (err: any, rows: any) => {
+                if (err) {
+                    console.error(err.message);
+                    return;
                 }
-                blocks.push(block);           
+                rows.forEach((element: any) => {
+                    let start: Date = new Date(element.start * 1000);
+                    let stop: Date = new Date(element.stop * 1000);
+                    let matches: number[] = []
+                    let block: ScheduleBlock = {
+                        start, stop, matches
+                    }
+                    blocks.push(block);           
+                });
+            });
+            // only get qual matches
+            db.each('SELECT match, projected_time FROM matches WHERE round = 2', (err: any, row: any) => {
+                for (let i = 0; i < blocks.length; i++) {
+                    if (blocks[i].start.getTime() <= row.projected_time * 1000 && row.projected_time * 1000 < blocks[i].stop.getTime()) {
+                        //console.log(`assigned Q${row.match} to block ${i}`);
+                        blocks[i].matches.push(row.match);
+                        break;
+                    }
+                }
+            }, (err: any, count: any) => {
                 resolve(blocks);
             });
-        });
+        })
     });
 }
