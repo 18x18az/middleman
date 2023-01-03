cb29d42502fc5b5e7a80d6528d72c0688d459277
Alec Miller <alecmichaelmiller@gmail.com>
2023-01-02 19:58:15 -0700

    Fixed handling for alliance awards

--- src/awards.ts
+++ src/awards.ts
@@ -1,24 +1,40 @@
-import { IAwards } from "@18x18az/rosetta";
+import { IAllianceTeams, IAward, IAwards, TeamId } from "@18x18az/rosetta";
 import { tm } from "./request";
 import { getTeamIdFromNumber } from "./teams";
 
 export async function getAwards(division: string): Promise<IAwards> {
+    const awardMap = new Map<string, TeamId | IAllianceTeams | null>();
+
     const raw = await tm.getTable(`${division}/awards`);
-    const winningTeams = raw.map((row: any) => {
+    raw.map((row: any) => {
         const id = row.id;
         const awardName = row.querySelector(`#${id}_name`).textContent;
         const awardWinnerSelector = row.querySelector(`#${id}_winnerEntryNumber`);
         const selectedIndex = awardWinnerSelector.selectedIndex;
         let winner = null;
-        if(selectedIndex){
+        if (selectedIndex) {
             const winnerNumber = awardWinnerSelector.options[selectedIndex].textContent;
             winner = getTeamIdFromNumber(winnerNumber);
         }
 
-        return {
-            name: awardName,
-            winner
+        const existing = awardMap.get(awardName);
+        if (existing) {
+            if (existing && winner) {
+                const winningAlliance: IAllianceTeams = {
+                    team1: existing as TeamId,
+                    team2: winner
+                }
+                awardMap.set(awardName, winningAlliance);
+            }
+        } else {
+            awardMap.set(awardName, winner);
         }
+    });
+
+    const winningTeams: IAwards = [];
+    awardMap.forEach((winner, name) => {
+        const award: IAward = {name, winner};
+        winningTeams.push(award)
     });
 
     return winningTeams;
